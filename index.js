245e3311cbae14b824200503d0c8d2b8a5629833
aidan-ma316 <77308659+aidanmara@users.noreply.github.com>
2024-08-09 17:12:00 -0700

    added about page

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
